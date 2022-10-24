4293ac73e6306c28f9818affd8ffdf131f77b46b
Sonia34sonia <sonia.vidal@sncf.fr>
2022-10-24 12:20:05 +0200

    commit block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockComponent } from '../app/block/block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   BlockComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/block/block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrls: ['./block.component.css']
+})
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export class BlockComponent {
+  title = 'ABC';
+}
